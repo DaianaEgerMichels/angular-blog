fef72ce7b26cc98ffef6e8a222be0dcc42d338cf
Daiana Michels <daianaem.12@gmail.com>
2023-10-12 16:40:26 -0300

    component menu-title updated

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
   styleUrls: ['./menu-title.component.css']
 })
-export class MenuTitleComponent {
+export class MenuTitleComponent implements OnInit {
 
+  text = "Daiana Eger Michels";
+  result: HTMLElement | null = null;
+
+  count = 0;
+
+  ngOnInit() {
+    this.toType();
+  }
+
+  // Insert letters one by one
+  toType() {
+    this.result = document.getElementById("title");
+    if (this.result) {
+      setTimeout(() => this.insert(this.text[this.count]), 200);
+    }
+  }
+  insert(letter: string) {
+    if (this.result) {
+      this.result.innerHTML += letter;
+      this.count++;
+      if (this.count < this.text.length) {
+        setTimeout(() => this.insert(this.text[this.count]), 200);
+      }
+    }
+  }
 }
